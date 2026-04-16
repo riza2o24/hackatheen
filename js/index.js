0c31038fb57d09b21529a03de2de04fa22f87457
Otabek <abdullayevriza80@gmail.com>
2026-04-16 14:22:33 +0500

    qwert

---
+++ js/index.js
@@ -0,0 +1,57 @@
+const API = "https://dummyjson.com/products";
+const box = document.getElementById("main__API");
+const input = document.getElementById("input2");
+
+let allProducts = [];
+
+fetch(API)
+  .then(res => res.json())
+  .then(data => {
+    allProducts = data.products;
+    showProducts(allProducts);
+  });
+
+function showProducts(products){
+  box.innerHTML = "";
+
+  products.slice(0,15).forEach(p => {
+
+    const card = document.createElement("div");
+    card.classList.add("productss");
+
+    card.innerHTML = `
+    
+    <div class="discount">${p.discountPercentage.toFixed(0)}%</div>
+
+    <img src="${p.thumbnail}" class="product-img">
+
+    <p class="category">${p.category}</p>
+
+    <h4 class="title">${p.title}</h4>
+
+    <div class="price-box">
+        <span class="price">$${p.price}</span>
+        <span class="old">$${Math.floor(p.price*1.2)}</span>
+    </div>
+
+    <button class="add">Add</button>
+    
+    `;
+
+    box.appendChild(card);
+  });
+}
+
+
+
+input.addEventListener("input", () => {
+
+  const value = input.value.toLowerCase();
+
+  const filtered = allProducts.filter(p =>
+    p.title.toLowerCase().includes(value)
+  );
+
+  showProducts(filtered);
+
+});
